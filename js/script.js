9bfc9f13aea71d530eff3bdf91b5aa5e20256cbf
elena <olenakunina@gmail.com>
2019-11-28 17:50:12 +0200

    schedule go on

--- js/script.js
+++ js/script.js
@@ -73,15 +73,19 @@ var scheduleHandler = scheduleTab.querySelector('.schedule__handler');
 var week = scheduleTab.querySelector('.schedule__list');
 var weekDays = scheduleTab.querySelector('.schedule__list');
 var allDaysSchedules = scheduleTab.querySelectorAll('.schedule__class-list-per-day');
-console.log(weekDays);
+//var scheduleElementHandler = scheduleTab.querySelector('.schedule__item-handler');
 
-var hideDaySchedule = function () {
+/*var hideDaySchedule = function () {
 	for (var i = 0; i < allDaysSchedules.length; i++) {
     	if (allDaysSchedules[i].classList.contains('schedule__class-list-per-day--shown')) {
     		allDaysSchedules[i].classList.remove('schedule__class-list-per-day--shown');
+    		var crossElement = allDaysSchedules[i].querySelector('.schedule__item-handler');
+    		if (crossElement.classList.contains('schedule__item-handler--shown')) {
+    			crossElement.classList.remove('schedule__item-handler--shown');
+    		}
     	}
     }
-};
+};*/
 
 scheduleHandler.addEventListener('click', function () {
 	week.classList.toggle('schedule__list--sown');
@@ -92,9 +96,31 @@ scheduleHandler.addEventListener('click', function () {
 weekDays.addEventListener('click', function (evt) {
     var target = event.target;
     
-    hideDaySchedule();
     var daySchedule = target.querySelector('.schedule__class-list-per-day');
+    var scheduleElementHandler = target.querySelector('.schedule__item-handler');
+    console.log(daySchedule);
     daySchedule.classList.add('schedule__class-list-per-day--shown');
+    scheduleElementHandler.classList.add('schedule__item-handler--shown');
+    
+    scheduleElementHandler.addEventListener('click', function () {
+		scheduleElementHandler.classList.remove('schedule__item-handler--shown');
+		daySchedule.classList.remove('schedule__class-list-per-day--shown');
+	});
 });
+
+// subscribing animation 
+
+var subscribing = document.querySelector('.subscribing');
+var subscribingButtons = subscribing.querySelectorAll('.subscribing__btn');
+var subscribingDescriptions = subscribing.querySelectorAll('.subscribing__longer-description');
+console.log(subscribingDescriptions);
+
+subscribing.addEventListener('click', function (evt) {
+    var target = event.target;
+
+    
+});
+
+
 
 
