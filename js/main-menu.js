edf0cf294d9d311776d713723e8a23495aae1055
elena <olenakunina@gmail.com>
2019-12-01 19:40:59 +0200

    some more pages

---
+++ js/main-menu.js
@@ -0,0 +1,9 @@
+//  menu mobile animation
+
+var menuToggler = document.querySelector('.main-nav__toggler');
+var menuBlock = document.querySelector('.main-nav__list');
+
+menuToggler.addEventListener('click', function () {
+	menuBlock.classList.toggle('main-nav__list--shown');
+	menuToggler.classList.toggle('main-nav__toggler--close');
+});
--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-//  menu mobile animation
-
-var menuToggler = document.querySelector('.main-nav__toggler');
-var menuBlock = document.querySelector('.main-nav__list');
-
-menuToggler.addEventListener('click', function () {
-	menuBlock.classList.toggle('main-nav__list--shown');
-	menuToggler.classList.toggle('main-nav__toggler--close');
-});
-
 // subscribtion mobile version handlers animation
 
 var subscribtionBlock = document.querySelector('.subscribing');
@@ -64,48 +54,6 @@ blogHandlerBack.addEventListener('click', function () {
 			}
 		}
 	}
-});
-
-// schedule block animation
-
-var scheduleTab = document.querySelector('.schedule');
-var scheduleHandler = scheduleTab.querySelector('.schedule__handler');
-var week = scheduleTab.querySelector('.schedule__list');
-var weekDays = scheduleTab.querySelector('.schedule__list');
-var allDaysSchedules = scheduleTab.querySelectorAll('.schedule__class-list-per-day');
-//var scheduleElementHandler = scheduleTab.querySelector('.schedule__item-handler');
-
-/*var hideDaySchedule = function () {
-	for (var i = 0; i < allDaysSchedules.length; i++) {
-    	if (allDaysSchedules[i].classList.contains('schedule__class-list-per-day--shown')) {
-    		allDaysSchedules[i].classList.remove('schedule__class-list-per-day--shown');
-    		var crossElement = allDaysSchedules[i].querySelector('.schedule__item-handler');
-    		if (crossElement.classList.contains('schedule__item-handler--shown')) {
-    			crossElement.classList.remove('schedule__item-handler--shown');
-    		}
-    	}
-    }
-};*/
-
-scheduleHandler.addEventListener('click', function () {
-	week.classList.toggle('schedule__list--sown');
-	scheduleHandler.classList.toggle('schedule__handler--close');
-});
-
-
-weekDays.addEventListener('click', function (evt) {
-    var target = event.target;
-    
-    var daySchedule = target.querySelector('.schedule__class-list-per-day');
-    var scheduleElementHandler = target.querySelector('.schedule__item-handler');
-    console.log(daySchedule);
-    daySchedule.classList.add('schedule__class-list-per-day--shown');
-    scheduleElementHandler.classList.add('schedule__item-handler--shown');
-    
-    scheduleElementHandler.addEventListener('click', function () {
-		scheduleElementHandler.classList.remove('schedule__item-handler--shown');
-		daySchedule.classList.remove('schedule__class-list-per-day--shown');
-	});
 });
 
 // subscribing animation 
