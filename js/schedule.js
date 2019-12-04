47eb5e961f6eb8d60c39cf010281c839591c0ec1
elena <olenakunina@gmail.com>
2019-12-04 14:27:46 +0200

    tablet version of home page

--- js/schedule.js
+++ js/schedule.js
@@ -3,15 +3,22 @@
 var scheduleTab = document.querySelector('.schedule');
 var scheduleHandler = scheduleTab.querySelector('.schedule__handler');
 var weekDays = scheduleTab.querySelector('.schedule__list');
-var allDaysSchedules = scheduleTab.querySelectorAll('.schedule__class-list-per-day');
-var dayScheduleHandler = scheduleTab.querySelector('.schedule__item-handler--shown');
-var daySchedule = scheduleTab.querySelector('.schedule__class-list-per-day--shown');
+var allWeekDays = scheduleTab.querySelectorAll('.schedule__item');
+//var dayScheduleHandler = scheduleTab.querySelector('.schedule__item-handler');
+//var daySchedule = scheduleTab.querySelector('.schedule__class-list-per-day');
 
 
 scheduleHandler.addEventListener('click', function () {
 	weekDays.classList.toggle('schedule__list--sown');
 	scheduleHandler.classList.toggle('schedule__handler--close');
 });
+
+var closeDaySchedule = function (evt) {
+    event.stopPropagation();
+    var target = event.target;
+    target.classList.remove('schedule__item-handler--shown');
+    daySchedule.classList.remove('schedule__class-list-per-day--shown');
+};
 
 var openDaySchedule = function (evt) {
     var target = event.target;
@@ -20,26 +27,29 @@ var openDaySchedule = function (evt) {
 
     daySchedule.classList.add('schedule__class-list-per-day--shown');
     dayScheduleHandler.classList.add('schedule__item-handler--shown');
+
+    dayScheduleHandler.addEventListener('click', closeDaySchedule);
 };
 
-weekDays.addEventListener('click', function (evt) {
-    var target = evt.target;
-    var daySchedule = target.querySelector('.schedule__class-list-per-day');
-    var dayScheduleHandler = target.querySelector('.schedule__item-handler');
+for (var i = 0; i < allWeekDays.length; i++) {
+    allWeekDays[i].addEventListener('click', function (evt) {
+        var target = event.currentTarget;
+        console.log(target);
+        var daySchedule = target.querySelector('.schedule__class-list-per-day');
+        var dayScheduleHandler = target.querySelector('.schedule__item-handler');
 
-    daySchedule.classList.add('schedule__class-list-per-day--shown');
-    dayScheduleHandler.classList.add('schedule__item-handler--shown');
+        daySchedule.classList.add('schedule__class-list-per-day--shown');
+        dayScheduleHandler.classList.add('schedule__item-handler--shown');
 
-    dayScheduleHandler.addEventListener('click', function (evt) {
-        event.stopPropagation();
-        var target = event.target;
-        target.classList.remove('schedule__item-handler--shown');
-        daySchedule.classList.remove('schedule__class-list-per-day--shown');
+        dayScheduleHandler.addEventListener('click', function (evt) {
+            event.stopPropagation();
+            var target = event.target;
+            target.classList.remove('schedule__item-handler--shown');
+            daySchedule.classList.remove('schedule__class-list-per-day--shown');
+        });
+        
     });
-});
-
-
-
+};
 
 /*var hideDaySchedule = function () {
     for (var i = 0; i < allDaysSchedules.length; i++) {
@@ -52,4 +62,3 @@ weekDays.addEventListener('click', function (evt) {
         }
     }
 };*/
-
