696e6690a46bdaaa6e23104f005b60317f5e9ad3
elena <olenakunina@gmail.com>
2019-12-08 00:46:40 +0200

    some fixings

--- js/subscribing.js
+++ js/subscribing.js
@@ -1,28 +1,24 @@
 // subscribtion mobile version handlers animation
 
 var subscribtionBlock = document.querySelector('.subscribing');
-var similarSubscribElements = document.querySelector('.subscribing__list');
 var blogHandlerForward = subscribtionBlock.querySelector('.control-handlers__handler--right');
 var blogHandlerBack = subscribtionBlock.querySelector('.control-handlers__handler--left');
 var subscribtions = subscribtionBlock.querySelectorAll('.subscribing__item');
 
-
-var findActiveItem = function (array) {
-	for (var i = 0; i < array.length; i++) {
+var findActiveItem = function () {
+	for (var i = 0; i < subscribtions.length; i++) {
 		var index;
-		if (array[i].classList.contains('subscribing__item--active')) {
+		if (subscribtions[i].classList.contains('subscribing__item--active')) {
 			index = i;
 		}
 	}
 	return index;
 };
-console.log(findActiveItem(subscribtions));
 
-var removeActiveElement = function (array) {
+var removeActiveElement = function () {
 	var index = findActiveItem();
-	array[index].classList.remove('subscribing__item--active');
+	subscribtions[index].classList.remove('subscribing__item--active');
 };
-removeActiveElement(subscribtions);
 
 blogHandlerForward.addEventListener('click', function () {
 	if (!blogHandlerForward.classList.contains('control-handlers__handler--faded')) {
@@ -67,9 +63,9 @@ blogHandlerBack.addEventListener('click', function () {
 // subscribing animation 
 
 for (var i = 0; i < subscribtions.length; i++) {
-	subscribtions[i].addEventListener('click', function () {
+	subscribtions[i].addEventListener('click', function (evt){
+		var target = event.currentTarget;
 		removeActiveElement();
-		subscribtions[i].classList.add('subscribing__item--active');
+		target.classList.add('subscribing__item--active');
 	});
-}
-
+};
